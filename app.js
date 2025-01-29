2d8ff093021354c99c89bfafa4a4c6a5dc6ed159
Madawee <Medhawyyy.7@hotmail.com>
2025-01-30 02:42:34 +0300

    edit file and use Node Js

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config;
+const express = require('express');
+
+/// help  create diffrent layouts for our website 
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+// help pass data through pages
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Static Files 
+app.use(express.static('public'));
+
+//Templating Engine 
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+//Router
+app.use('/' , require('./server/routes/index'))
+//app.use('/' , require('./server/routes/newpage'))   // هنا نحط الصفحة اللي اليوزر يدخل عليهاا ويشوف المفضلة (دقيقة 58 )
+
+//Handle 404 
+app.get('*', function(req,res) {
+    res.status(404).render('404');
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
+
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+/**
+ *  App Routes
+ */
+router.get('/', mainController.homepage)
+router.get('/about', mainController.about)
+router.get('/FAQ', mainController.FAQ)
+
+
+module.exports = router;
