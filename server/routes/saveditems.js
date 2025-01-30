8878ef10b4fa896fc6b5cf7c514ac792480e1d26
Madawee <Medhawyyy.7@hotmail.com>
2025-01-30 22:50:44 +0300

    New files

--- server/routes/saveditems.js
+++ server/routes/saveditems.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {isLoggedIn} = require('../middleware/checkAuth')
 const saveditemsController = require('../controllers/saveditemsController');
 
 /**
  *  SavedItems Routes 
  **/
+
+// add isLoggedIn to the get  1;46
 
 router.get('/saveditems', saveditemsController.saveditems)
 
